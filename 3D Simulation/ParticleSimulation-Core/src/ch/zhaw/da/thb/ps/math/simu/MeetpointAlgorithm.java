236853155a87a5f32eb7f7024d4433a409046378
Michael Hadorn <michael.hadorn@gmail.com>
2014-01-10 21:15:35 +0100

    meetpoint algorithm with cube

--- 3D Simulation/ParticleSimulation-Core/src/ch/zhaw/da/thb/ps/math/simu/MeetpointAlgorithm.java
+++ 3D Simulation/ParticleSimulation-Core/src/ch/zhaw/da/thb/ps/math/simu/MeetpointAlgorithm.java
@@ -8,7 +8,7 @@ package ch.zhaw.da.thb.ps.math.simu;
  * 
  *         Concrete Implementation of a simulation algorithm.
  */
-public class GravityAlgorithm extends BaseAlgorithm {
+public class MeetpointAlgorithm extends BaseAlgorithm {
 
     public int stopAt;
     private int speed;
@@ -20,13 +20,14 @@ public class GravityAlgorithm extends BaseAlgorithm {
 
     @Override
     public SimulationAlgorithm copy() {
-        GravityAlgorithm simuAlg = new GravityAlgorithm();
+        MeetpointAlgorithm simuAlg = new MeetpointAlgorithm();
 
         simuAlg.lastPs = lastPs;
         simuAlg.resultPs = resultPs;
         simuAlg.lowerBound = lowerBound;
         simuAlg.upperBound = upperBound;
         simuAlg.stopAt = stopAt;
+        simuAlg.speed = speed;
 
         return (BaseAlgorithm) simuAlg;
     }
@@ -47,12 +48,12 @@ public class GravityAlgorithm extends BaseAlgorithm {
                     if(Math.abs(resultPs.getCoordinates()[i]) >= this.stopAt){
                         float currentCor = resultPs.getCoordinates()[i];
                         int sign = (int) Math.signum(currentCor);
-                        resultPs.getCoordinates()[i] = currentCor - (sign * 50.0f);
+                        resultPs.getCoordinates()[i] = currentCor - (sign * (float)this.speed);
                     }
 
                     //resultPs.getColors()[i] = 0.8f;
 
-                    this.trackParticle(i, 26664);
+                    // this.trackParticle(i, 26664);
 			}
 
 			running = false;
@@ -66,13 +67,12 @@ public class GravityAlgorithm extends BaseAlgorithm {
      */
     public void setConfiguration(String mode) {
         if (mode == "cube") {
-            this.stopAt = 1500;
-            this.speed = 5;
+            this.stopAt = 2000;
+            this.speed = 25;
         } else {
             this.stopAt = 0;
-            this.speed = 10;
+            this.speed = 50;
         }
-
     }
 
 }
