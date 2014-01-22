dd7df3f3e53d666cc598e218a7b05130a1209fa2
Dani <daniel.brun@gmx.net>
2014-01-22 16:58:16 +0100

    Simulation Programmes

--- 3D Simulation/ParticleSimulation-Core/src/ch/zhaw/da/thb/ps/Runner.java
+++ 3D Simulation/ParticleSimulation-Core/src/ch/zhaw/da/thb/ps/Runner.java
@@ -4,8 +4,14 @@
 package ch.zhaw.da.thb.ps;
 
 import ch.zhaw.da.thb.SimulationStarter;
+import ch.zhaw.da.thb.ps.math.init.Grid2DInitAlgImpl;
 import ch.zhaw.da.thb.ps.math.init.Grid3DInitAlgImpl;
+import ch.zhaw.da.thb.ps.math.init.ParticleInitializeAlgorithm;
+import ch.zhaw.da.thb.ps.math.init.RandomParticleIniAlgImpl;
 import ch.zhaw.da.thb.ps.math.simu.BarnesHutAlgorithm;
+import ch.zhaw.da.thb.ps.math.simu.GravityAlgorithm;
+import ch.zhaw.da.thb.ps.math.simu.NBodyBruteForceAlgorithm;
+import ch.zhaw.da.thb.ps.math.simu.SimulationAlgorithm;
 import ch.zhaw.da.thb.ps.simulation.SimulationConfig;
 
 /**
@@ -18,50 +24,80 @@ public class Runner {
 	/**
 	 * 
 	 */
-	public Runner() {
-        // SimulationAlgorithm simuAlg = new RandomExampleAlgorithm();
-        // SimulationAlgorithm simuAlg = new MovingExampleAlgorithm();
-        // SimulationAlgorithm simuAlg = new MeetpointAlgorithm();
-        // simuAlg.setConfiguration("cube");
-		
-        // select an algorithm
-         //SimulationAlgorithm simuAlg = new NBodyBruteForceAlgorithm();
+	public Runner(int aProgrammCode) {
+		// SimulationAlgorithm simuAlg = new RandomExampleAlgorithm();
+		// SimulationAlgorithm simuAlg = new MovingExampleAlgorithm();
+		// SimulationAlgorithm simuAlg = new MeetpointAlgorithm();
+		// simuAlg.setConfiguration("cube");
 
-        //SimulationAlgorithm simuAlg = new GravityAlgorithm();
-         BarnesHutAlgorithm simuAlg = new BarnesHutAlgorithm();
+		SimulationAlgorithm simuAlg = null;
+		ParticleInitializeAlgorithm initAlg = null;
 
-        // select an particle system base
-         //ParticleInitializeAlgorithm initAlg = new RandomParticleIniAlgImpl();
-		// Grid3DInitAlgImpl initAlg = new Grid3DInitAlgImpl(10,2000);
-		Grid3DInitAlgImpl initAlg = new Grid3DInitAlgImpl(10,500,50,400);
-		// Grid2DInitAlgImpl initAlg = new Grid2DInitAlgImpl(500,5,100);
+		int particleCount = 2000;
 
+		switch (aProgrammCode) {
 
-		SimulationConfig config = new SimulationConfig(true, 500, 10, 5, simuAlg, initAlg);
+		// Gravity
+		case 1:
+			particleCount = 5000;
+			simuAlg = new GravityAlgorithm();
+			initAlg = new RandomParticleIniAlgImpl();
+			break;
+		case 2:
+			particleCount = 5000;
+			simuAlg = new GravityAlgorithm();
+			initAlg = new Grid2DInitAlgImpl(particleCount, 50, 100);
+			break;
+		case 3:
+			particleCount = 5000;
+			simuAlg = new GravityAlgorithm();
+			initAlg = new Grid3DInitAlgImpl(10, particleCount, 50, 400);
+			break;
+
+		// NBody
+		case 101:
+			particleCount = 650;
+			simuAlg = new NBodyBruteForceAlgorithm();
+			initAlg = new RandomParticleIniAlgImpl();
+			break;
+		case 102:
+			particleCount = 650;
+			simuAlg = new NBodyBruteForceAlgorithm();
+			initAlg = new Grid2DInitAlgImpl(particleCount, 50, 100);
+			break;
+		case 103:
+			particleCount = 650;
+			simuAlg = new NBodyBruteForceAlgorithm();
+			initAlg = new Grid3DInitAlgImpl(10, particleCount, 50, 400);
+			break;
+
+		// Barnes Hut
+		case 201:
+			particleCount = 1000;
+			simuAlg = new BarnesHutAlgorithm();
+			initAlg = new RandomParticleIniAlgImpl();
+			break;
+		case 202:
+			particleCount = 1000;
+			simuAlg = new BarnesHutAlgorithm();
+			initAlg = new Grid2DInitAlgImpl(particleCount, 50, 100);
+			break;
+		case 203:
+			particleCount = 1000;
+			simuAlg = new BarnesHutAlgorithm();
+			initAlg = new Grid3DInitAlgImpl(10, particleCount, 50, 400);
+			break;
+		default: {
+			simuAlg = new NBodyBruteForceAlgorithm();
+			initAlg = new RandomParticleIniAlgImpl();
+		}
+		}
+
+		SimulationConfig config = new SimulationConfig(true, particleCount, 10,
+				2, simuAlg, initAlg);
 
 		SimulationStarter starter = new SimulationStarter(config);
 		starter.start();
-
-		int programmCode = 0;
-		
-		switch(programmCode){
-		
-		//Gravity
-		case 1:
-			
-			break;
-			
-		//NBody
-		case 100:
-			
-			break;
-			
-		//Barnes Hut
-		case 200:
-			
-			break;
-			
-		}
 	}
 
 	/**
@@ -69,12 +105,12 @@ public class Runner {
 	 */
 	public static void main(String[] args) {
 		Integer progCount = Integer.valueOf(-1);
-		
-		if(args.length == 1){
-			
+
+		if (args.length == 1) {
+			progCount = Integer.parseInt(args[0]);
 		}
-		
-		new Runner();
+
+		new Runner(203);
 
 	}
 
